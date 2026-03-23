73e6bf1735090149cf9f9e7f5a81794e9164aff7
Alessio Chessa <ale.chessa28@gmail.com>
2026-03-23 21:07:45 +0100

    fix: set default value for last_login_at column in users table

--- src/api/database/migrations/000001_create_users_table.up.sql
+++ src/api/database/migrations/000001_create_users_table.up.sql
@@ -6,5 +6,5 @@ CREATE TABLE IF NOT EXISTS users (
 	google_id VARCHAR(255) UNIQUE NOT NULL,
 	created_at timestamptz DEFAULT NOW(),
 	updated_at timestamptz DEFAULT NOW(),
-	last_login_at timestamptz
+	last_login_at timestamptz DEFAULT NOW()
 );
